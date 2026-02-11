3150daac0542bdad18d89fb49c252caffb70da97
uygarpolat <uygarpolat@gmail.com>
2026-02-11 16:10:54 +0200

    finished updating event logic

--- models/event.go
+++ models/event.go
@@ -69,3 +69,14 @@ func GetEventById(id int64) (*Event, error) {
 	}
 	return &e, nil
 }
+
+func (e Event) Update() error {
+	query := "UPDATE events SET name = ?, description = ?, location = ?, dateTime = ? WHERE id = ?"
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.ID)
+	return err
+}
--- routes/routes.go
+++ routes/routes.go
@@ -8,4 +8,5 @@ func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events", getEvents)
 	server.GET("/events/:id", getEvent)
 	server.POST("/events", createEvent)
+	server.PUT("/events/:id", updateEvent)
 }
