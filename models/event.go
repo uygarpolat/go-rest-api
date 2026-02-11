c5b888538b824a3107c26a2f7c402a83c6dfcb20
uygarpolat <uygarpolat@gmail.com>
2026-02-12 01:37:07 +0200

    finished product

--- models/event.go
+++ models/event.go
@@ -89,3 +89,25 @@ func (e Event) Delete() error {
 	_, err = stmt.Exec(e.ID)
 	return err
 }
+
+func (e Event) Register(userId int64) error {
+	query := "INSERT INTO registrations (eventId, userId) VALUES (?, ?)"
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(e.ID, userId)
+	return err
+}
+
+func (e Event) CancelRegistration(userId int64) error {
+	query := "DELETE FROM registrations WHERE eventId = ? AND userId = ?"
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(e.ID, userId)
+	return err
+}
