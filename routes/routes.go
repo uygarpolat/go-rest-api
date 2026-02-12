e5f362777535029d8e8adeee123a7412f0809dc8
uygarpolat <uygarpolat@gmail.com>
2026-02-12 23:47:19 +0200

    Added CORS middleware so React can call the backend

--- routes/routes.go
+++ routes/routes.go
@@ -1,11 +1,21 @@
 package routes
 
 import (
-	"github.com/gin-gonic/gin"
 	"example.com/rest-api/middlewares"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 func RegisterRoutes(server *gin.Engine) {
+	server.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost:3000", "http://localhost:5173"},
+		AllowMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		MaxAge:       12 * time.Hour,
+	}))
+
 	server.GET("/events", getEvents)
 	server.GET("/events/:id", getEvent)
 
